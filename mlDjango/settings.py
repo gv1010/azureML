8602226fa2f7fca06f599b4a429b9fc1aaba9e24
gv1010 <gv281093@gmail.com>
2020-10-02 16:45:17 +0530

    Update settings.py

--- mlDjango/settings.py
+++ mlDjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7i+=ni=g=_*6*g19a7hkvey*4qcu=i6@tfdb0g8e6)rxd$v#q7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
