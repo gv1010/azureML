d11b93864d535f7953779368e514ec1ae47b1fbf
gv1010 <gv281093@gmail.com>
2020-10-02 12:22:55 +0530

    Add files via upload

--- mlapp/views.py
+++ mlapp/views.py
@@ -5,35 +5,35 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from mlapp.serializers import WineSerializers
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.svm import SVR
 from sklearn.pipeline import make_pipeline
+from rest_framework import status
 import numpy as np
 import pandas as pd
 import os
 import joblib
+from mlapp.models import Wine
 
 class WineView(APIView):
 	def get(self, request, *args, **kwargs):
-		return Response({'quality':"Success"})
-	
+		return Response(["Success"])
+		
 	def post(self, request, *args, **kwargs):
 		model_path = os.getcwd()+'\\model.joblib'
 		scaler_path = os.getcwd()+'\\scaler.joblib'
-		print(model_path)
 		print()
-		print()
-		print()
-		serializer = WineSerializers(data=request.data)
-		serializer.is_valid()
+		#serializer = WineSerializers(data=request.data)
+		#serializer.is_valid()
 		loaded_model = joblib.load(model_path)
 		scaler = joblib.load(scaler_path)
 		df = request.data
-		print(request.data)
-		print(list(map(float,list(df.values()))))
 		unit = np.array(list(map(float,list(df.values()))))
 		unit = unit.reshape(1, -1)
+		print()
+		print(unit)
 		unit_t = scaler.transform(unit)
+		print()
+		print(unit_t)
 		y_pred = loaded_model.predict(unit_t)
-		return Response({'quality':y_pred})
-		#return Response({"data not valid"})
+		print("Predicted Wine Quality:", y_pred)
+		return Response([y_pred], status=status.HTTP_200_OK)
